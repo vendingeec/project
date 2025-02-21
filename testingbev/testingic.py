65418ddb698f586dbb48fa877cbcfb6d127aab61
vendingeec <vendingmachineeec@gmail.com>
2025-02-21 11:46:54 +0530

    Delete testingbev directory

--- testingbev/testingic.py
+++
@@ -1,8 +0,0 @@
-import board
-import busio
-from adafruit_mcp230xx.mcp23017 import MCP23017
-
-i2c = busio.I2C(board.SCL, board.SDA)
-print("Initializing MCP23017...")
-mcp = MCP23017(i2c, address=0x24)
-print("MCP23017 detected and initialized.")
